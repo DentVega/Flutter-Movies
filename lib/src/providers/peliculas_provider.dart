1155b7444da5c50f8571347411210e2b796d86d1
DentVega <dentvega6@gmail.com>
2019-12-24 16:32:08 -0400

    - Optimizacion del Stream Builder para que evitar el cargado infinito de datos - Optimizacion del PageView, Se paso de un PageView a un PageViewBuilder para que el cargado de items de la lista se mas optimo de acuerdo al tamaño de la lista y solo cuando sea necesario

--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -9,6 +9,7 @@ class PeliculasProvider {
   String _url = 'api.themoviedb.org';
   String _languaje = 'es-ES';
   int _popularesPage = 0;
+  bool _cargando = false;
 
 
   List<Pelicula> _populares = new List();
@@ -44,6 +45,10 @@ class PeliculasProvider {
   }
 
   Future<List<Pelicula>> getPopulares() async {
+    if (_cargando) {
+      return [];
+    }
+    _cargando = true;
     _popularesPage++;
     final url = Uri.http(_url, '3/movie/popular', {
       'api_key': _apiKey,
@@ -53,6 +58,7 @@ class PeliculasProvider {
     final resp = await _procesarRespuesta(url);
     _populares.addAll(resp);
     popularesSink(_populares);
+    _cargando = false;
     return resp;
   }
 }
--- lib/src/widgets/movie_horizontal.dart
+++ lib/src/widgets/movie_horizontal.dart
@@ -6,23 +6,57 @@ class MovieHorizontal extends StatelessWidget {
   final Function siguientePagina;
 
   MovieHorizontal({@required this.peliculas, @required this.siguientePagina});
-  final _pageController = new PageController(initialPage: 1, viewportFraction: 0.3);
+
+  final _pageController =
+      new PageController(initialPage: 1, viewportFraction: 0.3);
 
   @override
   Widget build(BuildContext context) {
     final _screenSize = MediaQuery.of(context).size;
     _pageController.addListener(() {
-      if (_pageController.position.pixels >= _pageController.position.maxScrollExtent - 200) {
+      if (_pageController.position.pixels >=
+          _pageController.position.maxScrollExtent - 200) {
         siguientePagina();
       }
     });
 
     return Container(
       height: _screenSize.height * 0.25,
-      child: PageView(
+      child: PageView.builder(
         pageSnapping: false,
         controller: _pageController,
-        children: _targetas(context),
+//        children: _targetas(context),
+        itemCount: peliculas.length,
+        itemBuilder: (context, i) {
+          return _targeta(context, peliculas[i]);
+        },
+      ),
+    );
+  }
+
+  Widget _targeta(BuildContext context, Pelicula pelicula) {
+    return Container(
+      margin: EdgeInsets.only(right: 15.0),
+      child: Column(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: FadeInImage(
+              image: NetworkImage(pelicula.getPosterImg()),
+              placeholder: AssetImage('assets/img/no-image.jpg'),
+              fit: BoxFit.cover,
+              height: 130.0,
+            ),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Text(
+            pelicula.title,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.caption,
+          )
+        ],
       ),
     );
   }
