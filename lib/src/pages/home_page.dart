5c06f5335c4877a6460c6b48a3a60f71ddaa2805
DentVega <dentvega6@gmail.com>
2019-12-24 15:48:20 -0400

    - Uso de steambuild en el infinito scroll de las peliculas horizontalmente con ayuda del scrollController

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -8,6 +8,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    peliculasProvider.getPopulares();
     return Scaffold(
       appBar: AppBar(
         title: Text('Peliculas en Cines'),
@@ -63,11 +64,12 @@ class HomePage extends StatelessWidget {
           SizedBox(
             height: 5.0,
           ),
-          FutureBuilder(
-            future: peliculasProvider.getPopulares(),
+          StreamBuilder(
+            stream: peliculasProvider.popularesStream,
+
             builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
               if (snapshot.hasData) {
-                return MovieHorizontal(peliculas: snapshot.data);
+                return MovieHorizontal(peliculas: snapshot.data, siguientePagina: peliculasProvider.getPopulares,);
               } else {
                 return Center(
                   child: CircularProgressIndicator(),
