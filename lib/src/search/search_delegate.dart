82b97dfcc56bfaf436e0a37992cda301d08ec3fc
DentVega <dentvega6@gmail.com>
2019-12-24 20:57:24 -0400

    - BuildSuggestions y BuildResults

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
 class DataSearch extends SearchDelegate {
+  final peliculas = [
+    'Spiderman',
+    'Capitan America',
+    'Batman',
+    'Iron-Man',
+    'Iron-Man 2',
+    'Iron-Man 3',
+    'Shazam!',
+    'Superman'
+  ];
+  final peliculasRecientres = ['Spiderman', 'Capitan America'];
+  String seleccion = '';
+
   @override
   List<Widget> buildActions(BuildContext context) {
     // Las acciones de nuestro appBar
-    return [IconButton(icon: Icon(Icons.clear), onPressed: () {
-      print('CLICK!!');
-      query = '';
-    })];
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            print('CLICK!!');
+            query = '';
+          })
+    ];
   }
 
   @override
@@ -28,12 +45,36 @@ class DataSearch extends SearchDelegate {
   @override
   Widget buildResults(BuildContext context) {
     // Es el builder que crea los resultados que mostraremos
-    return Container();
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        color: Colors.blueAccent,
+        child: Text(seleccion),
+      ),
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // Son las sugerencias que aparecen cuando la persona escribe
-    return Container();
+    final listaSugerida = (query.isEmpty)
+        ? peliculasRecientres
+        : peliculas
+            .where((p) => p.toLowerCase().startsWith(query.toLowerCase()))
+            .toList();
+
+    return ListView.builder(
+        itemCount: listaSugerida.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(Icons.movie),
+            title: Text(listaSugerida[index]),
+            onTap: () {
+              seleccion = listaSugerida[index];
+              showResults(context);
+            },
+          );
+        });
   }
 }
